a984a3e70261bbe6598c999e9f6956a5349bcc62
Wolverin-e <patel.6@iitj.ac.in>
2021-01-19 01:34:39 +0530

    added signaling

---
+++ signaling/server.js
@@ -0,0 +1,49 @@
+const { Server } = require('socket.io');
+
+reg = {}
+
+class SignalingServer {
+
+	constructor(options = {}){
+		this.server = new Server(options);
+		this.bootstrap();
+	}
+
+	bootstrap(){
+		this.server.on("connection", soc => {
+
+			console.log(soc.id, "Connected");
+
+			soc.once("contactID", contactID => {
+				soc.contactID = contactID;
+				reg[contactID] = soc;
+				console.log(`${soc.id} connected as ${contactID}`);
+			});
+
+			soc.on("connectTo", connectTo => {
+				const socTo = reg[connectTo];
+				soc.on("transmit", msg => {
+					socTo.emit("transmission", msg);
+				});
+
+				socTo.on("transmit", msg => {
+					soc.emit("transmission", msg);
+				});
+				console.log(`${soc.contactID} connected to ${connectTo}`);
+			});
+
+			soc.on("disconnect", () => {
+				delete reg[soc.contactID]
+			});
+		});
+	}
+
+	listen(port = 8000){
+		this.server.listen(port)
+		console.log(`Signaling Server Listening over ${port}`);
+	}
+}
+
+new SignalingServer().listen(8000);
+
+// export default SignalingServer;
