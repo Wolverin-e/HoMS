afe3924ea62917cb70f0480fbb94c7d865a94872
Wolverin-e <patel.6@iitj.ac.in>
2021-01-24 18:06:31 +0530

    Some changes..

--- client-dashboard/src/reducers/roomservice-reducers.js
+++ client-dashboard/src/reducers/roomservice-reducers.js
@@ -5,29 +5,6 @@
 const roomServiceReducer = (state, action) => {
 
 	switch (action.type) {
-		// case CREATE_ROOM_SERVICE_REQ:
-		// 	return dispatch => {
-		// 		// api.post('/roomService/create', {...action.payload})
-		// 		// 	.then(res => {
-		// 		// 		console.log(res);
-		// 		// 		dispatch(showNotification({
-		// 		// 			head: "Service",
-		// 		// 			body: action.payload.type+" Request Created",
-		// 		// 			autohide: true
-		// 		// 		}))
-		// 		// 	})
-
-		// 		api.get('/roomService/fetchAll')
-		// 			.then(res => {
-		// 				console.log(res);
-		// 				dispatch(showNotification({
-		// 					head: "Service",
-		// 					body: action.payload.type+" Request Created",
-		// 					autohide: true
-		// 				}))
-		// 			});
-		// 		return state;
-		// 	}
 		default:
 			return state;
 	}
