b2a4f1ec16fe89d197dc502cd66d0fd58b7e1bda
Wolverin-e <patel.6@iitj.ac.in>
2021-01-25 17:32:51 +0530

    Refactored app.js.

--- api-server/app.js
+++ api-server/app.js
@@ -9,10 +9,7 @@ const setRoutes = require('./routes');
 const PORT = 8080;
 
 var app = Express();
-var jsonParser = bodyParser.json();
-
-app.use(jsonParser);
-
+app.use(bodyParser.json());
 app.use(cors({
 	origin: /.*/,
 	methods: ["GET", "POST", "OPTION"]
@@ -20,12 +17,12 @@ app.use(cors({
 
 setRoutes(app);
 
-const httpsServer = https.createServer({
-	key: fs.readFileSync("../certs/key.pem"),
-	cert: fs.readFileSync("../certs/cert.pem")
-}, app);
-
-httpsServer.listen(PORT, (err) => {
-	if(err) console.log(err);
-	console.log(`Listening on ${PORT}`);
-})
+https
+	.createServer({
+		key: fs.readFileSync("../certs/key.pem"),
+		cert: fs.readFileSync("../certs/cert.pem")
+	}, app)
+	.listen(PORT, (err) => {
+		if(err) console.log(err);
+		console.log(`Listening on ${PORT}`);
+	});
