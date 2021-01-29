00e06b0a2ddceb798113a6f07ff19ca66862f3cb
Wolverin-e <patel.6@iitj.ac.in>
2021-01-30 04:40:19 +0530

    Made rooms/fetchAll more informative.

--- api-server/routes/rooms/fetchAll.js
+++ api-server/routes/rooms/fetchAll.js
@@ -1,7 +1,13 @@
 const con = require('../con');
 
 module.exports = (req, res) => {
-	const sql = "SELECT * FROM rooms";
+	const sql = `SELECT rooms.room_no, rooms.type, rooms.status, occupies.customer_id \
+				 FROM rooms \
+				 LEFT JOIN occupies ON ( \
+					rooms.room_no=occupies.room_no AND \
+					occupies.arrival<=CURDATE() AND \
+					CURDATE()<=occupies.departure \
+				 )`;
 	con.query(sql, function(err, result, fields) {
 		if(err) throw err;
 		res.send(result);
