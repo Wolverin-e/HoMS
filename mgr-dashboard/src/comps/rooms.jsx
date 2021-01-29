78639fe88249787af0b394280debb8c9ded1d36d
Wolverin-e <patel.6@iitj.ac.in>
2021-01-30 04:40:58 +0530

    Added More categories.

--- mgr-dashboard/src/comps/rooms.jsx
+++ mgr-dashboard/src/comps/rooms.jsx
@@ -4,32 +4,72 @@ import { Col, Container, Row, FormControl, InputGroup, Button} from 'react-boots
 
 import { connect } from 'react-redux';
 import { fetchRooms } from '../actions/rooms-actions';
+import { fetchCustomers } from '../actions/customers-actions';
 import mapStateToProps from '../utils/mapStateToProps';
 
 
 class Rooms extends Component {
 
+	state={
+		room_no: null,
+		customer_id: null
+	}
+
 	componentDidMount(){
 		this.props.fetchRooms();
+		this.props.fetchCustomers();
+	}
+
+	onSelectChange = ({target}) => {
+		let x = {};
+		x[target.id] = target.value;
+		this.setState(x);
+	}
+
+	onClickAllot = () => {
+
 	}
 
 	render() {
+		const under_maintainance = this.props.rooms.filter(x => !x.status);
+		const available_rooms = this.props.rooms.filter(x => x.status && (x.customer_id === null));
+		const filled_rooms = this.props.rooms.filter(x => x.customer_id !== null);
 		return (
 			<Container className="pt-4">
-				{/* <Row md={5} className="justify-content-center pb-4">
-					<InputGroup className="align-self-center">
-						<FormControl placeholder="Room no"/>
+				{/* <Row className="justify-content-center pb-4">
+					<InputGroup as={Col}>
+						<FormControl id="room_no" as="select" onChange={this.onSelectChange}>
+							<option> Select Room </option>
+							{available_rooms.map((x, i) => 
+								<option key={i} value={x.room_no}>
+									{x.room_no+", "+x.type}
+								</option>
+							)}
+						</FormControl>
+						<FormControl id="customer_id" as="select" onChange={this.onSelectChange}>
+							<option> Select Customer </option>
+							{this.props.customers.map((x, i) => 
+								<option key={i} value={x.customer_id}>
+									{x.name+" : "+x.phone}
+								</option>
+							)}
+						</FormControl>
 						<InputGroup.Append>
-							<Button>Alpha</Button>
+							<Button onClick={this.onClickAllot}>Allot</Button>
 						</InputGroup.Append>
 					</InputGroup>
 				</Row> */}
 				<Row>
 					<Col>
-						<RoomsTable lable="FUNCTIONAL" rooms={this.props.rooms.filter(x => x.status)}/>
+						<RoomsTable lable="AVAILABLE" rooms={available_rooms}/>
 					</Col>
 					<Col>
-						<RoomsTable lable="UNDER MAINTAINANCE" rooms={this.props.rooms.filter(x => !x.status)}/>
+						<RoomsTable lable="UNDER MAINTAINANCE" rooms={under_maintainance}/>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<RoomsTable lable="FILLED ROOMS" rooms={filled_rooms}/>
 					</Col>
 				</Row>
 			</Container>
@@ -39,7 +79,8 @@ class Rooms extends Component {
 
 const mapDispatchToProps = dispatch => {
 	return {
-		fetchRooms: () => dispatch(fetchRooms())
+		fetchRooms: () => dispatch(fetchRooms()),
+		fetchCustomers: () => dispatch(fetchCustomers())
 	}
 }
 
